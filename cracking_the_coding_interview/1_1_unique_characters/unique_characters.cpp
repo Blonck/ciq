30596adeffc3d425fab2c06063ffb1aabc8aa7d6
Martin Marenz <martin.marenz+github@gmail.com>
2023-11-01 19:31:19 +0100

    [c++] cracking the coding interview 1.1: unique characters
    Add solution which needs no extra memory but runs in O(N log(N)).

--- cracking_the_coding_interview/1_1_unique_characters/unique_characters.cpp
+++ cracking_the_coding_interview/1_1_unique_characters/unique_characters.cpp
@@ -22,6 +22,25 @@ bool is_unique_chars_01(const std::string& input) {
     return true;
 }
 
+// time complexity: O(N log(N))
+// space complexity: O(1)
+// modifies input
+bool is_unique_chars_02(std::string& input) {
+    if(input.empty()){
+        return true;
+    }
+
+    std::sort(input.begin(), input.end());
+
+    for(std::size_t i=0; i < input.size()-1; ++i) {
+        if (input[i] == input[i+1]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 
 // test implementation
 
@@ -51,6 +70,31 @@ bool run_tests() {
         ASSERT(!fut("  "));
         ASSERT(!fut("1234567890abcdefghijklmnopqrstuvw1"));
         ASSERT(!fut("[["));
+    }
+
+    std::vector<std::function<bool(std::string&)>> futs2;
+    futs2.push_back([](std::string& input) {return is_unique_chars_02(input);});
+
+    for(auto& fut: futs2) {
+        std::string input = "";
+        ASSERT(fut(input));
+        input = "a";
+        ASSERT(fut(input));
+        input = "aA";
+        ASSERT(fut(input));
+        input = "1234567890abcdefghijklmnopqrstuvw";
+        ASSERT(fut(input));
+        input = " 1[](){}2";
+        ASSERT(fut(input));
+
+        input = "11";
+        ASSERT(!fut(input));
+        input = "  ";
+        ASSERT(!fut(input));
+        input = "1234567890abcdefghijklmnopqrstuvw1";
+        ASSERT(!fut(input));
+        input = "[[";
+        ASSERT(!fut(input));
     }
 
     return true;
